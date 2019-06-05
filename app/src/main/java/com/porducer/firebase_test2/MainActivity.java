97e8e3b44b3a028972685ff340db455136f604c9
noareyenoar <noarlplp@gmail.com>
2019-06-05 20:33:37 +0700

    Add All FB Login Guide Done Without UI update

--- app/src/main/java/com/porducer/firebase_test2/MainActivity.java
+++ app/src/main/java/com/porducer/firebase_test2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.porducer.firebase_test2;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     String Login;
     private static final String EMAIL = "email";
     private CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         final Button button = (Button) findViewById(R.id.button);
         final Button button2 = (Button) findViewById(R.id.button2);
         LoginButton buttonFacebookLogin = findViewById(R.id.buttonFacebookLogin);
@@ -62,7 +65,26 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(Read_user_Listener);
 
         mCallbackManager = CallbackManager.Factory.create();
-        buttonFacebookLogin.setOnClickListener(FB_Login_Listener);
+        buttonFacebookLogin.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(Context,"FB Login Done!",Toast.LENGTH_LONG).show();
+                //handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(Context,"FB Login Calcle!!",Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(Context,"FB Login error!!",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 
     private View.OnClickListener Add_user_Listener = new View.OnClickListener(){
@@ -72,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             user.put("Password", "kittiphon");
             user.put("Date_of_register",new Timestamp(new Date()));
             Login = (String) user.get("Login");
-            //
+
             user_ref = DB.Data_add_user_regis(user);
             Toast.makeText(Context,"User add DONE",Toast.LENGTH_LONG).show();
         }
@@ -87,13 +109,36 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private View.OnClickListener FB_Login_Listener = new View.OnClickListener(){
-        public void onClick(View v){
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Toast.makeText(Context,"handleFB token Done!",Toast.LENGTH_LONG).show();
 
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Context,"FB Credential Done!",Toast.LENGTH_LONG).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Context,"FB Credential Failed!!",Toast.LENGTH_LONG).show();
+                            //updateUI(null);
+                        }
 
+                    }
+                });
+    }
 
-        }
 
 
 
